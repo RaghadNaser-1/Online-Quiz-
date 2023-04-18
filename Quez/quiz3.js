e6282fbaa004a0f694e87c5d58f749d0d2919bd5
RaghadNaser-1 <126399047+RaghadNaser-1@users.noreply.github.com>
2023-04-17 19:30:50 -0700

    brief 3 quiz site

---
+++ Quez/quiz3.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+  numb: 1,
+  question: "The 'function' and 'var' are known as",
+  answer: "Declaration statements",
+  options: [
+    "Declaration statements",
+    "Data types",
+    "	Keywords",
+    "Boolean"
+  ]
+},
+  {
+  numb: 2,
+  question: "In JavaScript the x===y statement implies that:?",
+  answer: "Both are equal in the value and data type.t",
+  options: [
+    "Both x and y are equal in value, type and reference address as well.",
+    "Both are x and y are equal in value only.",
+    "	Both are equal in the value and data type.",
+    "Both are not same at all."
+  ]
+},
+  {
+  numb: 3,
+  question: "Choose the correct snippet from the following to check if the variable 'a' is not equal the 'NULL':",
+  answer: "if(a!==null)",
+  options: [
+    "if(a!null)",
+    "if (a!) ",
+    "if(a!==null)",
+    "if(a!=null)"
+  ]
+},
+  {
+  numb: 4,
+  question: "A collection of elements of the same data type which may either in order or not, is called ?",
+  answer: "Array",
+  options: [
+    "String",
+    "Serialized Object",
+    "Object",
+    "Array",
+  ]
+},
+  {
+  numb: 5,
+  
+  question: "How can a datatype be declared to be a constant type?",
+  answer: "const",
+  options: [
+    "const",
+    "var",
+    "let",
+    "constant",
+  ]
+},]
